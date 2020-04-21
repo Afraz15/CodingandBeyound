7877a91a56b6f003dadf8c19775ee46176e58cb5
Afraz15 <62801628+Afraz15@users.noreply.github.com>
2020-04-22 02:41:08 +0500

    Add files via upload

--- pattern matching and with regular expression/21.py
+++ pattern matching and with regular expression/21.py
@@ -3,18 +3,13 @@
 import re,pyperclip
 
 phone = re.compile(r'''(
-    (\d{3}|\(\d{3}\)))?             # area code
-    (\s|-|\.)?                      # seperator 
-    (\d{3})                         # first 3 digits
-    (\s|-|\.)                       # seperator
-    (\d{4})                         # last 4 digits
-    (\s*(ext|x|ext.)\s*(\d{2,3}))?  # extension
-    )''',re.VERBOSE)
-
-
-
-
-
+    (\d{3}|\(\d{3}\))?                # area code
+    (\s|-|\.)?                        # separator
+    (\d{3})                           # first 3 digits
+    (\s|-|\.)                         # separator
+    (\d{4})                           # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
+    )''', re.VERBOSE)
 
 '''
 in the area code , the code can be any 3 digits and if it is in parameters then the other one will be used and we seperate them with piper or 'bitwise or' operator
